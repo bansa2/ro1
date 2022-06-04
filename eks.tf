6328d0ab47e636b0724e0f7f60896631d3f29ca4
bansa2 <ayushbansal506@gmail.com>
2022-06-04 14:18:55 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "aws_eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.ps_1.id,aws_subnet.ps_1.id]
+    subnet_ids = [aws_subnet.ps_1.id,aws_subnet.ps_2.id]
   }
   tags = {
     "Name" = "EKS_ayush"
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node_ayush"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = [aws_subnet.ps_1.id,aws_subnet.ps_1.id]
+  subnet_ids      = [aws_subnet.ps_1.id,aws_subnet.ps_2.id]
   scaling_config {
     desired_size = 1
     max_size     = 1
